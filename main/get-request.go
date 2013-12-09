2ec87b42e1189f27b55aa0dd7384a0e8af505fc5
snorwood <steven@snorwood.com>
2013-12-09 15:23:02 -0500

    Messing up json parsing...

--- main/get-request.go
+++ main/get-request.go
@@ -11,11 +11,7 @@ func mp3Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func musicHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `
-		<audio controls>
-			<source src="/mp3/" type="audio/mpeg">
-			<embed height="50" width="100" src="/music/StudioEIM/The Third Flight/MapleStory/Wize">
-		</audio>`)
+	fmt.Fprint(w, `{\"artist\": \"MOOOOO\"}`)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
