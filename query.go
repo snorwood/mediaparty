15484e5efef95270b6c427f0279fa224b6c48839
snorwood <steven@snorwood.com>
2014-01-12 13:55:21 -0500

    Template in place for providing song search filters

--- query.go
+++ query.go
@@ -4,42 +4,41 @@ import (
 	"fmt"
 )
 
+// rowScanner encompasses all row objects with scan functionality
 type rowScanner interface {
 	Scan(...interface{}) error
 }
 
+// Table contains the schema.table data
 type Table struct {
 	Schema string
 	Table  string
 }
 
-func GetSongQuery(schema, table, artist, title, album, albumArtist string) (string, error) {
-	song := Song{
-		Artist:      artist,
-		Title:       title,
-		Album:       album,
-		AlbumArtist: albumArtist,
-	}
-
+// GetSongQuery builds the query for a single song
+func GetSongQuery(schema, table string, song Song) (string, error) {
+	// Make sure the song info is valid
 	if !song.Valid() {
 		return "", InvalidSong(song)
 	}
 
+	// Create/validate the table object
 	songTable := Table{
 		Schema: schema,
 		Table:  table,
 	}
-
 	if schema == "" || table == "" {
 		return "", fmt.Errorf("Invalid Table:\nschema=%s\ntable=%s", schema, table)
 	}
 
+	// Create the columns list
 	columns := struct {
 		Columns []string
 	}{
 		Columns: []string{},
 	}
 
+	// Build up the query
 	querySegment := ""
 	var err error = nil
 
@@ -59,6 +58,60 @@ func GetSongQuery(schema, table, artist, title, album, albumArtist string) (stri
 	return query, nil
 }
 
+// VariableSongQuery creates a query out of only the given song data.
+func VariableSongQuery(schema, table string, song Song) (string, error) {
+	// Build and verify the table
+	songTable := Table{
+		Schema: schema,
+		Table:  table,
+	}
+	if schema == "" || table == "" {
+		return "", fmt.Errorf("Invalid Table:\nschema=%s\ntable=%s", schema, table)
+	}
+
+	// Build the column list
+	columns := struct {
+		Columns []string
+	}{
+		Columns: []string{},
+	}
+
+	// Build the query up
+	querySegment := ""
+	var err error = nil
+
+	query := ""
+
+	querySegment, err = ExecuteTemplate(SelectColumnsTemplate, columns)
+	query += querySegment
+	querySegment, err = ExecuteTemplate(FromTableTemplate, songTable)
+	query += querySegment
+
+	// Add filter data Artist -> Album -> Title
+	query += "WHERE "
+
+	if song.Artist == "" {
+		return query, err
+	}
+
+	query += formatColumn("Artist") + " = " + formatValue(song.Artist)
+
+	if song.Album == "" {
+		return query, err
+	}
+
+	query += " AND " + formatColumn("Album") + " = " + formatValue(song.Album)
+
+	if song.Title == "" {
+		return query, err
+	}
+
+	query += " AND " + formatColumn("Title") + " = " + formatValue(song.Title)
+
+	return query, err
+}
+
+// StringToPostgresString converts a normal string into a postgres compatible one.
 func StringToPostgresString(s string) string {
 	for i := 0; i < len(s); i++ {
 		escapeCharNext := (i < len(s)-1 && (string(s[i+1]) == "\\" || string(s[i+1]) == "'"))
--- templates.go
+++ templates.go
@@ -1,9 +1,12 @@
 package mediaparty
 
 import (
+	"fmt"
+	"io/ioutil"
 	"text/template"
 )
 
+// writableString is a string that can be wrote to using writers
 type writableString string
 
 func (self *writableString) Write(p []byte) (int, error) {
@@ -15,26 +18,45 @@ func (self *writableString) Reset() {
 	*self = ""
 }
 
+// templateDefinition is a string type that specifies it is to be used for templates
 type templateDefinition string
 
+// SelectColumnsTemplate defines the columns to select
 var SelectColumnsTemplate templateDefinition = `
 	Select {{parseColumns .Columns}}
 	`
 
+// FromTableTemplate defines the table to select from
 var FromTableTemplate templateDefinition = `
 	FROM {{.Schema}}.{{.Table}}
 	`
 
+// SongWhereTemplate defines the song info
 var SongWhereTemplate templateDefinition = `
 	Where
 		LOWER("Artist") = LOWER('{{.Artist}}') AND
 		LOWER("Title")  = LOWER('{{.Title}}') AND
 		LOWER("Album")  = LOWER('{{.Album}}')
 	`
+
+// MusicPlayer returns the html template for an embedded music player
+func MusicPlayer() templateDefinition {
+	content, err := ioutil.ReadFile("../musicplayer.html")
+	html := string(content)
+
+	if err != nil {
+		fmt.Printf("Could not find the musicplayer definition: %s", err)
+	}
+
+	return templateDefinition(html)
+}
+
+// funcMap maps function to be used in templates
 var funcMap template.FuncMap = template.FuncMap{
 	"parseColumns": parseColumns,
 }
 
+// ExecuteTemplate returns a copy of the template filled in with the object.
 func ExecuteTemplate(templateString templateDefinition, object interface{}) (string, error) {
 	temp := template.Must(template.New("temp").Funcs(funcMap).Parse(string(templateString)))
 	resultReader := new(writableString)
@@ -46,6 +68,7 @@ func ExecuteTemplate(templateString templateDefinition, object interface{}) (str
 	return string(*resultReader), nil
 }
 
+// parseColumns creates a Postgres formatted list of columns. * if no columns passed.
 func parseColumns(columns []string) string {
 	if len(columns) == 0 {
 		return "*"
@@ -56,4 +79,14 @@ func parseColumns(columns []string) string {
 		parsedString += ", " + columns[i]
 	}
 	return parsedString
+}
+
+// formatColumn provides Postgres column definition formatting to a normal string
+func formatColumn(column string) string {
+	return "\"" + column + "\""
+}
+
+// formatValue provides Postgres string value formatting to a normal string
+func formatValue(value string) string {
+	return "'" + value + "'"
 }
